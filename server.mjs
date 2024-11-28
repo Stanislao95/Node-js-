15978ba21b577f355d6744e3ae064571d67ddc20
Stanislao95 <166648506+Stanislao95@users.noreply.github.com>
2024-11-28 20:17:31 +0100

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -3,8 +3,9 @@ import {createServer} from "node.http";
 const server = createServer((request , response) =>{
     console.log("response recived")
     response.statusCode= 200;
-    response.setHeader(`Content-Type`, `text/html`);
+    response.setHeader(`Content-Type`, `appliction/jcson`);
     response.end("<html><body><h1>Questa pagina è sul sevrver di Node.js</h1></body></html>");
+    const jsonResponseBody = JSON.stringify({location: "Mars"})
 });
 
 server.listen(3000, ()=>{
